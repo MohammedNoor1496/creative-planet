c8e1fdded0523676948d3a7a3c18e323e35876cf
MohNooR1496 <62880535+MohammedNoor1496@users.noreply.github.com>
2022-05-12 15:27:12 +0300

    update theme colors

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 
-const kSecondaryColor = Colors.orange;
-const kGreenColor = Color(0xFF6AC259);
+const kSecondaryColor = Color.fromARGB(255, 85, 3, 99);
+const kGreenColor = Color.fromARGB(255, 126, 197, 112);
 const kRedColor = Color(0xFFE92E30);
 const kGrayColor = Color(0xFFC1C1C1);
 const kBlackColor = Color(0xFF101010);
 const kPrimaryGradient = LinearGradient(
-  colors: [Color(0xFF46A0AE), Color(0xFF00FFCB)],
+  colors: [Color.fromARGB(255, 85, 3, 99), Color.fromARGB(255, 126, 197, 112)],
   begin: Alignment.centerLeft,
   end: Alignment.centerRight,
 );
